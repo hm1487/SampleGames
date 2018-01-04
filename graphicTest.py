32a6a1035ef11fc2a4a1334697291ded1d0d9424
hm1487 <hm1487@nyu.edu>
2018-01-03 21:10:10 -0500

    Working with Graphics

---
+++ graphicTest.py
@@ -0,0 +1,31 @@
+import pygame as game
+
+game.init()
+
+#Defining Variables
+WHITE = (255,255,255)
+BLUE = (   0,   0, 255)
+done = False
+clock = game.time.Clock()
+size = (700,500)
+
+
+game.display.set_caption("EAT THIS ASS BOI")
+screen = game.display.set_mode(size)
+
+while not done:
+	for event in game.event.get(): # getting what the user did
+		if (event.type ==game.QUIT):
+			print("You are trying to quit")
+			done = True
+		elif (event.type == game.KEYDOWN):
+			print("You pressed a key")
+		elif (event.type == game.KEYUP):
+			print("You released a key")
+		elif (event.type == game.MOUSEBUTTONDOWN):
+			print("You pressed on the mouse")
+		screen.fill(BLUE)	
+		game.display.flip()
+		clock.tick(60)
+
+game.quit()
---
+++ shapes.py
@@ -0,0 +1,32 @@
+import pygame as game
+
+game.init()
+
+#Defining Variables
+WHITE = (255,255,255)
+BLUE = (   0,   0, 255)
+GREEN = ( 0, 255, 0)
+done = False
+y_offset = 0
+clock = game.time.Clock()
+size = (700,500)
+font = game.font.SysFont('Calibri', 25, True, False)
+text = font.render("NAAAAANI",True, WHITE)
+
+game.display.set_caption("EAT THIS ASS BOI")
+screen = game.display.set_mode(size)
+
+while not done:
+	for event in game.event.get(): # getting what the user did
+		if (event.type == game.QUIT):
+			print("You are trying to quit") #i COULD USE THIS FOR A KEYLOGGER
+			done = True
+		
+		screen.fill(BLUE)
+		game.draw.line(screen,GREEN,[0,0+y_offset],[100,100+y_offset],5) #lmao this looks funny
+		y_offset += 10
+		screen.blit(text,[300,250])
+		game.display.flip()
+		clock.tick(60)
+
+game.quit()
